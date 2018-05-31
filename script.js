a28f22b3c16ea61d24a7a0578c375b3c5924b9e8
David Fuka <gravetotkd@gmail.com>
2018-05-30 18:39:59 -0700

    New commit

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-angular.module("myApp", ["ngMaterial", "ngMessages"]);
+angular.module("myApp", ["ngMaterial", "ngMessages"])
+  .controller("MainCtrl", function(){
+    this.items = [
+      {
+        label: "item1",
+        description: "some item"
+      },
+      {
+        label: "item2",
+        description: "second item"
+      },
+      {
+        label: "item3",
+        description: "third item"
+      }
+    ];
+  });
